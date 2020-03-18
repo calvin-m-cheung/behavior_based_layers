c6135daceb9524688718c878d6d467a5bdc6708e
theeternalcowboy <calvin.cheung@gmail.com>
2020-03-18 18:56:09 -0400

    fixing git submodule issues

---
+++ navigation/vfh_local_planner/include/vfh_local_planner/vfh_local_planner_ros.h
@@ -0,0 +1,100 @@
+#ifndef VFH_LOCAL_PLANNER_ROS_H_
+#define VFH_LOCAL_PLANNER_ROS_H_
+
+#include <iostream>
+
+#include <ros/ros.h>
+
+// abstract class from which our plugin inherits
+#include <nav_core/base_local_planner.h>
+
+// transforms
+#include <angles/angles.h>
+#include <tf/tf.h>
+#include <tf/transform_listener.h>
+
+// costmap & geometry
+#include <costmap_2d/costmap_2d_ros.h>
+
+#include <base_local_planner/local_planner_util.h>
+#include <base_local_planner/goal_functions.h>
+#include <base_local_planner/latched_stop_rotate_controller.h>
+
+// msgs
+#include <nav_msgs/Path.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <visualization_msgs/Marker.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <geometry_msgs/Twist.h>
+
+// config and parameters
+#include <dynamic_reconfigure/server.h>
+#include "vfh_local_planner/vfh_local_plannerConfig.h"
+
+#include "vfh_local_planner/vfh_local_planner.h"
+
+
+namespace vfh_local_planner 
+{
+
+    class VFHPlannerRos : public nav_core::BaseLocalPlanner
+    {
+    public:
+        VFHPlannerRos();
+
+        VFHPlannerRos(std::string name, tf::TransformListener* tf,
+          costmap_2d::Costmap2DROS* costmap_ros);
+        
+        ~VFHPlannerRos();
+
+        void initialize(std::string name, tf::TransformListener* tf,
+          costmap_2d::Costmap2DROS* costmap_ros);
+
+        bool setPlan(const std::vector<geometry_msgs::PoseStamped>& orig_global_plan);
+        
+        bool computeVelocityCommands(geometry_msgs::Twist& cmd_vel);
+
+        bool isGoalReached();
+
+    private:
+
+        int GetPlanPoint(std::vector<geometry_msgs::PoseStamped> transformed_plan, std::vector<geometry_msgs::PoseStamped> &global_plan, tf::Stamped<tf::Pose> current_pose);
+
+        void reconfigureCB(vfh_local_plannerConfig &config, uint32_t level);
+
+        // pointer to external objects (do NOT delete object)
+        costmap_2d::Costmap2DROS* costmap_ros_; ///<@brief pointer to costmap
+        costmap_2d::Costmap2D* costmap_;
+        tf::TransformListener* tf_; ///<@brief pointer to Transform Listener
+        std::vector<geometry_msgs::PoseStamped> global_plan_;
+        base_local_planner::OdometryHelperRos odom_helper_;
+        dynamic_reconfigure::Server<vfh_local_plannerConfig> *dsrv_;
+        vfh_local_planner::vfh_local_plannerConfig default_config_;
+        vfh_local_planner::vfh_local_plannerConfig config_;
+        std::string global_frame_;
+
+        VFHPlanner vfh_planner;
+
+        int vfh_sections_number;
+        int smooth_length;
+        double vfh_threshold;
+        double yaw_goal_tolerance_, xy_goal_tolerance_;
+        double previews_direction;
+        
+
+        // flags
+        bool rotating_to_goal_;
+        bool xy_goal_latch_;
+        bool finding_alternative_way_;
+        bool initialized_;
+        bool goal_reached_;
+
+        //to publish info to ROS system
+        ros::Publisher VFF_vector_pose_pub;
+    };
+
+}
+
+
+#endif
---
+++ navigation/vfh_local_planner/src/vfh_local_planner_ros.cpp
@@ -0,0 +1,347 @@
+#include "vfh_local_planner/vfh_local_planner_ros.h"
+
+#include <pluginlib/class_list_macros.h>
+
+PLUGINLIB_DECLARE_CLASS(vfh_local_planner, VFHPlannerRos, vfh_local_planner::VFHPlannerRos, nav_core::BaseLocalPlanner)
+
+namespace vfh_local_planner
+{
+    std::string ns = ros::this_node::getNamespace();
+    std::string odom_name = ns.erase(0,1)==""?"odom":ns.erase(0,1)+"_tf/odom";
+
+    VFHPlannerRos::VFHPlannerRos(): costmap_ros_(NULL), tf_(NULL), initialized_(false), odom_helper_(odom_name) {};
+
+    VFHPlannerRos::VFHPlannerRos(std::string name, tf::TransformListener* tf, costmap_2d::Costmap2DROS* costmap_ros)
+        : costmap_ros_(NULL), tf_(NULL), initialized_(false), odom_helper_(odom_name)
+    {
+        // initialize planner
+		initialize(name, tf, costmap_ros);
+
+    };
+
+    VFHPlannerRos::~VFHPlannerRos() {};
+
+
+    void VFHPlannerRos::initialize(std::string name, tf::TransformListener* tf, costmap_2d::Costmap2DROS* costmap_ros)
+    {
+        // check if plugin initialized
+        if(!initialized_)
+		{
+            ROS_INFO("Initializing VFH Planner");
+
+            ros::NodeHandle private_nh("~/" + name);
+
+            //Parameter for dynamic reconfigure
+            dsrv_ = new dynamic_reconfigure::Server<vfh_local_plannerConfig>(private_nh);
+            dynamic_reconfigure::Server<vfh_local_plannerConfig>::CallbackType cb = boost::bind(&VFHPlannerRos::reconfigureCB, this, _1, _2);
+            dsrv_->setCallback(cb);
+
+            xy_goal_latch_ = false;
+            rotating_to_goal_ = false;
+            finding_alternative_way_ = false;
+
+            costmap_ros_ = costmap_ros;
+            tf_ = tf;
+
+            global_frame_ = costmap_ros_->getGlobalFrameID();
+            costmap_ = costmap_ros_->getCostmap();
+            initialized_ = true;
+
+            vfh_planner.Initialize(costmap_);     
+
+            // Ready variables for publishing VFF Vector
+            VFF_vector_pose_pub = private_nh.advertise<visualization_msgs::Marker>("VFF_vector",1);
+        }
+        else
+        {
+            ROS_WARN("This planner has already been initialized, doing nothing.");
+        }
+        
+    }
+
+    void VFHPlannerRos::reconfigureCB(vfh_local_plannerConfig &config, uint32_t level)
+    {
+        if (config.restore_defaults)
+        {
+            config = default_config_;
+            config.restore_defaults = false;
+        }
+        config_ = config;
+        vfh_planner.Reconfigure(config);
+        goal_reached_ = false;
+    }
+
+    bool VFHPlannerRos::setPlan(const std::vector<geometry_msgs::PoseStamped>& orig_global_plan)
+    {
+        // check if plugin initialized
+		if(!initialized_)
+		{
+		ROS_ERROR("This planner has not been initialized, please call initialize() before using this planner");
+		return false;
+		}
+
+        ROS_INFO("Got new plan!");
+        //reset the global plan
+        global_plan_.clear();
+        global_plan_ = orig_global_plan;
+        xy_goal_latch_ = false;
+        rotating_to_goal_ = true;
+        goal_reached_ = false;
+        finding_alternative_way_ = false;
+            
+        return true;
+    };
+
+    bool VFHPlannerRos::computeVelocityCommands(geometry_msgs::Twist& cmd_vel)
+    {
+
+        ROS_INFO("Compute Velocity!");
+
+        //Check if plugin initialized
+		if(!initialized_)
+		{
+		ROS_ERROR("This planner has not been initialized, please call initialize() before using this planner");
+		return false;
+		}
+
+        //Get the pose of the robot in the global frame of the costmap
+        tf::Stamped<tf::Pose> current_pose;
+        if (!costmap_ros_->getRobotPose(current_pose)) {
+            std::cout << "cant get current pose" << std::endl;
+            return false;
+        }
+
+
+        //Get current robot velocity
+        tf::Stamped<tf::Pose> current_vel;
+        odom_helper_.getRobotVel(current_vel);
+
+        //Create transform used to transform coordinates from global planner frame to costmap frame
+        tf::StampedTransform frame_transform;
+        tf_->lookupTransform(global_frame_, ros::Time(), global_plan_.back().header.frame_id, global_plan_.back().header.stamp, 
+          global_plan_.back().header.frame_id, frame_transform);
+
+        //Transform plan goal from the global planner frame to the frame of the costmap
+        const geometry_msgs::PoseStamped& plan_goal = global_plan_.back();
+        tf::Stamped<tf::Pose> global_goal;
+        tf::poseStampedMsgToTF(plan_goal, global_goal);
+        global_goal.setData(frame_transform * global_goal);
+        global_goal.stamp_ = frame_transform.stamp_;
+        global_goal.frame_id_ = global_frame_;
+        
+        //Transforms the global plan of the robot from the global planner frame to the frame of the costmap
+        std::vector<geometry_msgs::PoseStamped> transformed_plan;
+        if (!base_local_planner::transformGlobalPlan(*tf_, global_plan_, current_pose, *costmap_, global_frame_, transformed_plan)) {
+            
+            ROS_WARN("Could not transform the global plan to the frame of the controller");
+            if (!config_.ignore_global_plan_updates)
+                return false;
+        }
+
+        tf::Stamped<tf::Pose> intermediary_goal_point;
+        //Check if the modified plan is not empty
+        if (!transformed_plan.empty())
+        {
+            ROS_INFO("Plan isn't empty");
+            //Trim off parts of the global plan that are far enough behind the robot
+            //base_local_planner::prunePlan(current_pose, transformed_plan, global_plan_);
+
+            //Get intermediary goal point in the transformed plan
+            int point_index = GetPlanPoint(transformed_plan, global_plan_,current_pose);
+            tf::poseStampedMsgToTF(transformed_plan.at(point_index), intermediary_goal_point);
+
+        }
+        else
+        {
+            ROS_ERROR("Plan is empty");
+            if (!config_.ignore_global_plan_updates)
+                return false;
+        }
+        ROS_INFO("Try to update Historgram");
+        //Update VFH histogram with new costmap
+        if (!vfh_planner.UpdateHistogram(costmap_ros_->getCostmap()))
+        {
+            ROS_WARN("Could not find clear direction");
+            return false;
+        }
+
+        //#############################################################################################################################################
+        //########################################### Check if the robot reached the goal position ####################################################
+
+
+        ROS_INFO("Check if the robot recahed the goal position");
+
+        //Check if the robot is at the goal coordinate x y
+        double goal_x = global_goal.getOrigin().getX();
+        double goal_y = global_goal.getOrigin().getY();
+        std::cout << "goal_x: "<<goal_x<<std::endl;
+        std::cout << "goal_y: "<<goal_y<<std::endl;
+
+        if (base_local_planner::getGoalPositionDistance(current_pose, goal_x, goal_y) <= config_.xy_goal_tolerance_ || xy_goal_latch_)
+        {
+            std::cout << "xy reached" << std::endl;
+            xy_goal_latch_ = true;
+            //Check if the robot is at the same orientation of the goal
+            double goal_th = tf::getYaw(global_goal.getRotation());
+            if (fabs(base_local_planner::getGoalOrientationAngleDifference(current_pose, goal_th)) <= config_.yaw_goal_tolerance_)
+            {
+                std::cout << "parou yupi" << std::endl;
+                cmd_vel.linear.x = 0.0;
+                cmd_vel.linear.y = 0.0;
+                cmd_vel.angular.z = 0.0;
+                goal_reached_ = true;
+                return true;
+            }
+            else{
+                std::cout << "rotating to goal to end" << std::endl;
+                vfh_planner.RotateToGoal(current_pose, current_vel, goal_th, cmd_vel);
+                return true;
+            }
+        }
+        //#############################################################################################################################################
+        //######################################################## Drive to the goal ##################################################################
+        ROS_INFO("Drive to goal");
+
+        double direction_to_follow;
+        double goal_distance;
+
+        /*
+         * if needing to find an alternate way, looks at global goals point. Otherwise, use an intermediate one. Note that the global goal point is the master list
+         * and for intermediate, that list is trimmed down of thins that are too far away.
+         */
+        if(finding_alternative_way_)
+        {
+        	ROS_INFO("Finding alternate way");
+            double global_plan_goal_angle = atan2((global_goal.getOrigin().getY()-current_pose.getOrigin().getY()), (global_goal.getOrigin().getX()-current_pose.getOrigin().getX()));
+            goal_distance = 0.2;
+            if (vfh_planner.DirectionIsClear(global_plan_goal_angle))
+            {
+                direction_to_follow = global_plan_goal_angle;
+                goal_distance = std::min(sqrt(pow((global_goal.getOrigin().getX()-current_pose.getOrigin().getX()),2)+pow((global_goal.getOrigin().getY()-current_pose.getOrigin().getY()),2)), 0.3);
+            }
+            else
+            {
+                direction_to_follow = vfh_planner.GetNewDirection(global_plan_goal_angle, tf::getYaw(current_pose.getRotation()),previews_direction);
+            }
+        }
+        else
+        {
+        	ROS_INFO("No alternate way needed");
+
+            double intermediary_goal_orientation = atan2((intermediary_goal_point.getOrigin().getY()-current_pose.getOrigin().getY()), (intermediary_goal_point.getOrigin().getX()-current_pose.getOrigin().getX()));
+
+            //Check if the path is free
+            if (!vfh_planner.DirectionIsClear(intermediary_goal_orientation))
+            {
+            	ROS_INFO("Direction is not clear");
+                finding_alternative_way_ = true;
+                cmd_vel.linear.x = 0.0;
+                cmd_vel.linear.y = 0.0;
+                cmd_vel.angular.z = 0.0;
+                return true;
+            }
+            else
+            {
+            	ROS_INFO("Direction is clear");
+                direction_to_follow = intermediary_goal_orientation;
+                goal_distance = sqrt(pow((intermediary_goal_point.getOrigin().getX()-current_pose.getOrigin().getX()),2)+pow((intermediary_goal_point.getOrigin().getY()-current_pose.getOrigin().getY()),2));
+            }
+        }
+
+        previews_direction = direction_to_follow;
+
+        std::cout << "Going to direction: " << radToDeg(direction_to_follow) << std::endl;
+        if (rotating_to_goal_)
+        {
+            std::cout << "rotating to goal to start" << std::endl;
+            vfh_planner.RotateToGoal(current_pose, current_vel, direction_to_follow, cmd_vel);
+            if (fabs(base_local_planner::getGoalOrientationAngleDifference(current_pose, direction_to_follow)) < config_.yaw_goal_tolerance_)
+            {
+                rotating_to_goal_ = false;
+            }
+            return true;
+        }
+        //Check if the robot is deviating too much from the plan
+        else if (fabs(base_local_planner::getGoalOrientationAngleDifference(current_pose, direction_to_follow)) > M_PI/3)
+        {
+            std::cout << "rotating to goal to start" << std::endl;
+            rotating_to_goal_ = true;
+            cmd_vel.linear.x = 0.05;
+            cmd_vel.linear.y = 0.0;
+            cmd_vel.angular.z = 0.0;
+        }
+        //Drive toward the plan
+        else 
+        {
+        	double drive_angle = angles::shortest_angular_distance(tf::getYaw(current_pose.getRotation()),direction_to_follow);
+        	std::cout << "driving to goal" << std::endl;
+            std::cout << "goal_distance:                          " << goal_distance << std::endl;
+            std::cout << "drive_angle:                            " << drive_angle << std::endl;
+            std::cout << "direction to follow:                    " << direction_to_follow << std::endl;
+            std::cout << "cos(direction_to_follow)*goal distance: " << cos(direction_to_follow)*goal_distance<< std::endl;
+            std::cout << "sin(direction_to_follow)*goal distance: " << sin(direction_to_follow)*goal_distance<< std::endl;
+            vfh_planner.DriveToward(drive_angle, goal_distance, cmd_vel);
+
+
+
+        }
+
+        visualization_msgs::Marker VFF_vector_msg;
+		VFF_vector_msg.header.frame_id = odom_name;
+		VFF_vector_msg.header.stamp = ros::Time();
+		VFF_vector_msg.ns = ros::this_node::getNamespace();
+		VFF_vector_msg.id = 0;
+		VFF_vector_msg.type = visualization_msgs::Marker::ARROW;
+		VFF_vector_msg.action = visualization_msgs::Marker::ADD;
+		VFF_vector_msg.scale.x = 0.1;
+		VFF_vector_msg.scale.y = 0.1;
+		VFF_vector_msg.scale.z = 0.1;
+		VFF_vector_msg.color.a = 1.0; // Don't forget to set the alpha!
+		VFF_vector_msg.color.r = 0.0;
+		VFF_vector_msg.color.g = 1.0;
+		VFF_vector_msg.color.b = 1.0;
+
+		geometry_msgs::Point p;
+		p.x= current_pose.getOrigin().x();
+		p.y= current_pose.getOrigin().y();
+		p.z= 0;
+		VFF_vector_msg.points.push_back(p);
+		p.x= current_pose.getOrigin().x()+cos(direction_to_follow)*goal_distance*10;
+		p.y=current_pose.getOrigin().y()+sin(direction_to_follow)*goal_distance*10;
+		p.z= 0;
+		VFF_vector_msg.points.push_back(p);
+		VFF_vector_pose_pub.publish(VFF_vector_msg);
+        return true;
+    };
+
+    bool VFHPlannerRos::isGoalReached()
+    {
+        // check if plugin initialized
+		if(!initialized_)
+		{
+		ROS_ERROR("This planner has not been initialized, please call initialize() before using this planner");
+		return false;
+		}
+        return goal_reached_;
+    };
+
+    int VFHPlannerRos::GetPlanPoint(std::vector<geometry_msgs::PoseStamped> transformed_plan, std::vector<geometry_msgs::PoseStamped> &global_plan, tf::Stamped<tf::Pose> current_pose)
+    {
+        int point = 0;
+        for (int i = 0; i < transformed_plan.size(); i++)
+        {
+            double point_distance = sqrt(pow((transformed_plan.at(i).pose.position.x-current_pose.getOrigin().getX()),2)+pow((transformed_plan.at(i).pose.position.y-current_pose.getOrigin().getY()),2));
+            if (point_distance < 0.6)
+            {
+                point = i;
+            }
+            else
+            {
+                if(point)
+                    break;
+            }
+            
+        }
+        return point;
+    }
+}
